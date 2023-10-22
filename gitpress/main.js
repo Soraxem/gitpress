7360269a9d81ce22e8723a06ed1def531e9e5731
soraxem <samuel.hafen@aol.com>
2023-10-22 22:43:50 +0700

    Added code to make text bold

--- gitpress/main.js
+++ gitpress/main.js
@@ -2,6 +2,21 @@
 layout: none
 ---
 
-function editor() {
-  var inputElement = document.getElementById("gitpress-editor").setAttribute("contenteditable","");
+function create_editor(content_id) {
+  if (document.getElementById(content_id).getAttribute("contenteditable")) {
+    document.getElementById(content_id).removeAttribute("contenteditable");
+  } else {
+    document.getElementById(content_id).setAttribute("contenteditable","true");
+  }   
+}
+
+function edit_strong(content_id) {
+  if (document.getElementById(content_id).getAttribute("contenteditable")) {
+    var sel = window.getSelection();
+    var range = sel.getRangeAt(0);
+    var newParent = document.createElement("strong");
+    range.surroundContents(newParent);
+    
+    var ulTag = range.commonAncestorContainer;
+  }
 }
